0149154b9b510890db091ab44e8fd7984961bb20
Mendel E <mendelmaleh@gmail.com>
2022-04-03 11:01:07 -0400

    Add initial capitalone parser

---
+++ parser/capitalone/types.go
@@ -0,0 +1,25 @@
+package capitalone
+
+import "time"
+
+type RawTransaction struct {
+	TransactionDate Date    `csv:"Transaction Date"`
+	PostedDate      Date    `csv:"Posted Date"`
+	CardNumber      string  `csv:"Card No."`
+	Description     string  `csv:"Description"`
+	Category        string  `csv:"Category"`
+	Debit           float64 `csv:"Debit,omitempty"`
+	Credit          float64 `csv:"Credit,omitempty"`
+}
+
+type Date time.Time
+
+func (d *Date) UnmarshalText(data []byte) error {
+	t, err := time.Parse("2006-01-02", string(data))
+	if err != nil {
+		return err
+	}
+
+	*d = Date(t)
+	return nil
+}
