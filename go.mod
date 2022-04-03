3e1ff508cd0a916b58a1043ade92173efbc99148
Mendel E <mendelmaleh@gmail.com>
2022-04-03 10:38:38 -0400

    Add initial amex parser

---
+++ go.mod
@@ -0,0 +1,8 @@
+module git.sr.ht/~mendelmaleh/pfin
+
+go 1.17
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/jszwec/csvutil v1.6.0
+)
---
+++ parser/amex/types.go
@@ -0,0 +1,80 @@
+package amex
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+type RawTransaction struct {
+	Date                     time.Time `csv:"Date"`
+	Description              string    `csv:"Description"`
+	CardMember               string    `csv:"Card Member"`
+	AccountNumber            string    `csv:"Account #"`
+	Amount                   float64   `csv:"Amount"`
+	ExtendedDetails          string    `csv:"Extended Details"`
+	AppearsOnYourStatementAs string    `csv:"Appears On Your Statement As"`
+	Address                  string    `csv:"Address"`
+
+	CityState `csv:"City/State"`
+
+	ZipCode   string `csv:"Zip Code"`
+	Country   string `csv:"Country"`
+	Reference string `csv:"Reference"`
+
+	Category `csv:"Category"`
+}
+
+type CityState struct {
+	City  string
+	State string
+}
+
+func (c *CityState) UnmarshalText(data []byte) error {
+	if len(data) == 0 {
+		return nil
+	}
+
+	s := strings.Split(string(data), "\n")
+
+	if len(s) >= 1 {
+		c.City = s[0]
+	}
+
+	if len(s) == 2 {
+		c.State = s[1]
+	}
+
+	if len(s) > 2 {
+		return fmt.Errorf("expected two parts for City/State, got %d (%v, %s)", len(s), s, string(data))
+	}
+
+	return nil
+}
+
+type Category struct {
+	Main string
+	Sub  string
+}
+
+func (c *Category) UnmarshalText(data []byte) error {
+	if len(data) == 0 {
+		return nil
+	}
+
+	s := strings.Split(string(data), "-")
+
+	if len(s) >= 1 {
+		c.Main = s[0]
+	}
+
+	if len(s) == 2 {
+		c.Sub = s[1]
+	}
+
+	if len(s) > 2 {
+		return fmt.Errorf("expected two parts for Category, got %d (%v, %s)", len(s), s, string(data))
+	}
+
+	return nil
+}
