b98717e1da4f9cbe04aef4dc87f3ace87625fb3c
Mendel E <mendelmaleh@gmail.com>
2022-04-04 03:12:15 -0400

    Switch capitalone parse function to use util parsedirs

--- parser/capitalone/parser.go
+++ parser/capitalone/parser.go
@@ -1,49 +1,20 @@
 package capitalone
 
 import (
-	"os"
-	"path/filepath"
-	"sort"
-
 	"git.sr.ht/~mendelmaleh/pfin"
 	"github.com/jszwec/csvutil"
 )
 
-func ParseDir(path string) ([]pfin.Transaction, error) {
-	var txns []pfin.Transaction
-
-	matches, err := filepath.Glob(path + "*.csv")
-	if err != nil {
-		return txns, err
+func Parse(data []byte, txns *[]pfin.Transaction) error {
+	var raw []RawTransaction
+	if err := csvutil.Unmarshal(data, &raw); err != nil {
+		return err
 	}
 
-	// sort oldest first
-	sort.Strings(matches)
-
-	for _, f := range matches {
-		file, err := os.ReadFile(f)
-		if err != nil {
-			return txns, err
-		}
-
-		var raw []RawTransaction
-		if err := csvutil.Unmarshal(file, &raw); err != nil {
-			return txns, err
-		}
-
-		// https://github.com/golang/go/wiki/SliceTricks#extend-capacity
-		/*
-			if cap(txns) - len(txns) < len(raw) {
-				txns = append(make([]pfin.Transaction, 0, len(txns) + len(raw)), txns...)
-			}
-		*/
-
-		// https://go.dev/doc/faq#convert_slice_of_interface
-		// append in reverse
-		for i := len(raw) - 1; i != 0; i-- {
-			txns = append(txns, raw[i])
-		}
+	// reverse order so it's chronological
+	for i := len(raw) - 1; i != 0; i-- {
+		*txns = append(*txns, raw[i])
 	}
 
-	return txns, nil
+	return nil
 }
