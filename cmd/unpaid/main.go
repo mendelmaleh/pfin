5d017f4ea5fd1030758b6de70b49dfb917b8493c
Mendel E <mendelmaleh@gmail.com>
2022-05-01 16:19:28 -0400

    cmd/unpaid: Sort transactions by date, calculate paid and total, skip account and user

--- cmd/unpaid/main.go
+++ cmd/unpaid/main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 	"text/tabwriter"
 
 	"git.sr.ht/~mendelmaleh/pfin"
@@ -26,6 +29,7 @@ func main() {
 	flag.StringVar(&opts.Users.String, "user", "", "filter user")
 	flag.StringVar(&opts.Accounts.String, "account", "", "filter account")
 	flag.StringVar(&opts.Payments.String, "payment", "payments", "filter payments account")
+
 	flag.StringVar(&opts.Separator, "sep", "\t", "separator")
 
 	flag.Parse()
@@ -61,24 +65,53 @@ func main() {
 		}
 	}
 
+	sort.SliceStable(txns, func(i, j int) bool {
+		return txns[i].Date().Before(txns[j].Date())
+	})
+
 	credit *= -1
 	// fmt.Printf("credit: %.2f\n", credit)
 
 	tw := tabwriter.NewWriter(os.Stdout, 0, 8, 1, '\t', 0)
+
+	var total, paid float64
 	for _, tx := range txns {
 		a := tx.Amount()
 
 		// print credits and uncovered debits
 		if a < 0 || a > credit {
-			fmt.Fprintln(tw, pfin.TxString(tx, opts.Separator))
+			total += a
+
+			fmt.Fprintln(tw, strings.Join([]string{
+				tx.Date().Format(pfin.ISO8601),
+				strconv.FormatFloat(tx.Amount(), 'f', 2, 64),
+				tx.Name(),
+				tx.Category(),
+			}, opts.Separator))
+		}
+
+		// skip credits
+		if a < 0 {
+			continue
+		}
+
+		// calculate paid
+		if a < credit {
+			paid += a
 		}
 
 		// sub debits
-		if a > 0 {
-			credit -= a
-		}
+		credit -= a
 	}
 
+	sep := opts.Separator
+	fmt.Fprint(tw, strings.Join([]string{
+		"",
+		fmt.Sprintf("total:%s%.2f%s((at least partially) unpaid and payments)", sep, total, sep),
+		fmt.Sprintf("paid:%s%.2f%s(completely covered by payments)", sep, paid, sep),
+		fmt.Sprintf("balance:%s%.2f", sep, credit),
+		"",
+	}, "\n"))
+
 	tw.Flush()
-	fmt.Printf("\ntotal: %.2f\n", credit)
 }
