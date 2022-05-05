80671e909d8fb0ee84802ff7f59991dbc8d92201
Mendel E <mendelmaleh@gmail.com>
2022-05-05 04:07:36 -0400

    Fix date spacing hack

--- parser/personal/types.go
+++ parser/personal/types.go
@@ -13,7 +13,7 @@ type Fields struct {
 }
 
 type Raw struct {
-	Date        util.DateISO `csv:"date    "`
+	Date        util.DateISO `csv:"date"`
 	Amount      float64      `csv:"amount"`
 	Media       string       `csv:"media"`
 	Description string       `csv:"description"`
